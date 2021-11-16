b3e1c689d558512b91cc43dc4375039881d36acf
unknown <clintonufere@gmail.com>
2021-11-17 00:32:04 +0100

    Css Update

--- public/js/index.js
+++ public/js/index.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
             let [y, m, d] = date.split("-");
             let [h, mm] = time.split(":");
             Time = `${d} ${getDateStr(m, month)}, ${y} - ${h % 12}:${mm}`;
-            let Img = "https://source.unsplash.com/1200x900/?" + name;
+            let Img = "https://source.unsplash.com/1200x900/?cloud";
 
 
             //Header
@@ -137,5 +137,5 @@ $(document).ready(function () {
 
     });
 
-    fetchWeather("Abuja");
+    fetchWeather("lagos");
 })
